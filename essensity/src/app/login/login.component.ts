6a2bf9d565b5037212be046859f1a71160f83d70
name = ER!CK <38144008+ERICK-ZABALA@users.noreply.github.com>
2022-12-28 01:27:40 +0000

    update

---
+++ essensity/src/app/login/login.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+// Add libraries firebase for Authentication
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+}
